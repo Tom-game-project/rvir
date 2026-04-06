47349433709c2cfa5ed01104d8cc638d17f7c336
muranaka tomonari <tom.ipynb@gmail.com>
2026-04-06 13:24:29 +0900

    「到達可能な定義」の導出を実装

--- src/ir/ir.rs
+++ src/ir/ir.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::marker::PhantomData;
+use std::os::linux::raw::stat;
 
 use crate::unit::size::Byte;
 
@@ -160,12 +161,16 @@ pub struct AllSetted;
 
 pub struct BasicBlockList<State> {
     pub list: Vec<BasicBlock>,
+    pub statement_list: StatementList,
     _phantom: PhantomData<State>
 }
 
 impl BasicBlockList<Setting> {
     pub fn new() -> Self {
-        Self { list: Vec::new(), _phantom: PhantomData }
+        Self { 
+            list: Vec::new(),
+            statement_list: StatementList { statements: Vec::new() },
+            _phantom: PhantomData }
     }
 
     pub fn alloc(&mut self, label: Label) -> BasicBlockId {
@@ -177,7 +182,13 @@ impl BasicBlockList<Setting> {
                 pred: Vec::new(), 
                 succ: Vec::new(), 
                 insts: Vec::new(), 
-                reach: HashSet::new() 
+                vreg_state_set: VregStateSet { 
+                    reach: Vec::new(),
+                    def: Vec::new(),
+                    kill: Vec::new(),
+                },
+                // 定義、生存区間解析用
+                statement_id_list: Vec::new(),
             }
         );
         index
@@ -189,7 +200,11 @@ impl BasicBlockList<Setting> {
 
     /// irの設定が終了したら、状態を移す
     pub fn finish_ir_setting (self) -> BasicBlockList<IRSetted> {
-        BasicBlockList { list: self.list, _phantom: PhantomData }
+        BasicBlockList { 
+            list: self.list,
+            statement_list: StatementList { statements: Vec::new() },
+            _phantom: PhantomData 
+        }
     }
 }
 
@@ -197,7 +212,15 @@ pub enum BasicBlockListError {
     UndefinedLabel(Label)
 }
 
-fn labelmap2idmap(label_map: HashMap<&Label, Vec<&Label>>, basic_block_list: &BasicBlockList<IRSetted>) -> Result<HashMap<BasicBlockId, Vec<BasicBlockId>>, BasicBlockListError> {
+// =================================================================================
+//                   helper function for BasicBlockList<IRSetted>
+// =================================================================================
+
+fn labelmap2idmap(
+    label_map: HashMap<&Label, Vec<&Label>>, 
+    basic_block_list: &BasicBlockList<IRSetted>
+) -> Result<HashMap<BasicBlockId, Vec<BasicBlockId>>, BasicBlockListError> 
+{
     Ok(label_map
         .iter()
         .map(|(&k, v)| {
@@ -227,6 +250,57 @@ fn labelmap2idmap(label_map: HashMap<&Label, Vec<&Label>>, basic_block_list: &Ba
         ))
 }
 
+fn generate_and_set_statement_ids(j: &Instruction, statement_list: &mut StatementList) -> Option<StatementId> {
+    match j { 
+        Instruction::Call { dest, func:_func, args } => {
+            let statement_id = statement_list
+                .gen_vreg_statement(
+                    *dest,
+                    args
+                    .iter()
+                    .filter_map(|operand| 
+                        match *operand {
+                            Operand::Const(_) => None,
+                            Operand::Reg(vreg)=> Some(vreg)
+                    })
+                    .collect::<Vec<VReg>>());
+            Some(statement_id)
+        }
+
+        Instruction::BinOp { op: _op, dest, lhs, rhs } => {
+            let mut p_uses_x =  Vec::new();
+            if let Operand::Reg(vreg) = lhs {
+                p_uses_x.push(*vreg);
+            }
+            if let Operand::Reg(vreg) = rhs {
+                p_uses_x.push(*vreg);
+            }
+            let statement_id = statement_list
+                .gen_vreg_statement(
+                    Some(*dest),
+                    p_uses_x);
+            Some(statement_id)
+        }
+
+        Instruction::Assign { dest, src } => {
+            let statement_id = statement_list
+                .gen_vreg_statement(
+                    Some(*dest),
+                    if let Operand::Reg(vreg) = src { 
+                        vec![*vreg] 
+                    } else { 
+                        Vec::new()
+                    }
+                );
+            Some(statement_id)
+        }
+
+        _ => {
+            None
+        }
+    }
+}
+
 /// IRの設定が終わったときに存在するメソッド
 impl BasicBlockList<IRSetted> {
 
@@ -234,7 +308,7 @@ impl BasicBlockList<IRSetted> {
         &self.list[id.0]
     }
 
-    pub fn get_basic_block_by_label(&self, label: &Label) -> Option<&BasicBlock>{
+    pub fn get_basic_block_by_label(&self, label: &Label) -> Option<&BasicBlock> {
         self.list.iter().find(|b | b.label == *label)
     }
 
@@ -267,20 +341,142 @@ impl BasicBlockList<IRSetted> {
         let pred_dict = labelmap2idmap(pred_dict, &self)?;
         let succ_dict = labelmap2idmap(succ_dict, &self)?;
 
-        for i in &mut self.list {
-            i.pred = pred_dict
-                .get(&i.id)
+        for basic_block in &mut self.list {
+            basic_block.pred = pred_dict
+                .get(&basic_block.id)
                 .unwrap() // 一旦全てのラベルに対して初期化しているので、ここは安全なunwrap
                 .clone();
 
-            i.succ = succ_dict
-                .get(&i.id)
+            basic_block.succ = succ_dict
+                .get(&basic_block.id)
                 .unwrap() // 一旦全てのラベルに対して初期化しているので、ここは安全なunwrap
                 .clone();
+
+            for j in &basic_block.insts {
+                // 式を「定義」、または「使用」するような命令について、statement_idを割り当てる
+                if let Some(statement_id) = generate_and_set_statement_ids(j, &mut self.statement_list /* 発行されたstatement_idに対応付けられたデータはここに保存される */) {
+                    basic_block.statement_id_list.push(statement_id);
+                }
+            }
+        }
+
+        // self.statement_listですべてのブロック内に存在する文の収集が完了する
+
+        // def, kill,の初期化
+
+        // DEF(B)の設定
+        // ある変数xについてp(def(x)) ∈ Bで且つpの後にp'(def(x)) ∈ Bなるp'が無い
+        // Bの中で定義され、Bの出口まで有効な文の集合
+        for basic_block in &mut self.list {
+            basic_block.vreg_state_set = VregStateSet::new(&self.statement_list);
+
+            // DEF(B:Block): Set<StatementId>
+            let mut def_statement = Vec::new();
+            // currentについて、DEF(B)になるかどうかを調べ、満たしていれば
+            for (i, current) in basic_block.statement_id_list.iter().enumerate() {
+                // 現在のインデックス + 1 から最後までを切り出す
+                let rest = &basic_block.statement_id_list[i + 1..];
+
+                let current_vreg_statement = self.statement_list.get_vreg_statement_by_id(*current);
+
+                if let Some(current_vreg) = current_vreg_statement.p_def_x {
+                    // 現在の文が変数を定義しているとき
+                    if rest.iter().any(|j| {
+                        let succ_vreg_statement = self.statement_list.get_vreg_statement_by_id(*j);
+                        if let Some(succ_vreg) = succ_vreg_statement.p_def_x {
+                            current_vreg == succ_vreg
+                        } else {
+                            false
+                        }
+                    }) {
+                        // 後続する同じブロック内に同じ仮想レジスタを上書きする定義がある
+                    } else {
+                        // 後続する同じブロック内に同じ仮想レジスタを上書きする定義がない
+                        def_statement.push(*current);
+                    }
+                } else {
+                    // 現在の変数は何も定義していない
+                }
+                // println!("処理中: {}, 後続のデータ: {:?}", current, rest);
+            }
+            basic_block.vreg_state_set.set_def(def_statement);
+
+            // KILL(B)の設定
+            // ブロック内で宣言された変数を上書きするような文の集合
+        }
+
+        // Kill(B)の設定
+        //
+        // 自分のブロック内で定義された変数を上書きするような文の集合
+        //
+        let statement_id_list = self.list.iter().map(|basic_block| {
+            // DEF(B:Block): Set<StatementId>
+            let vregs_defined_in_this_blcok = basic_block
+                .statement_id_list
+                .iter()
+                .fold(Vec::<VReg>::new(), |mut acc, statement_id| {
+                    let vreg_statement = self.statement_list.get_vreg_statement_by_id(*statement_id);
+                    if let Some(defined_vreg) = vreg_statement.p_def_x {
+                        acc.push(defined_vreg);
+                    }
+                    acc
+                });
+
+            let kill_statement = self.list
+                .iter()
+                .fold(Vec::<StatementId>::new(), |mut acc, b| {
+                    // 自分を含まないブロックのなかで、同じレジスタが上書きされている場合はその文を追加
+                    if basic_block.id != b.id {
+                        for other_statement_id in &b.statement_id_list {
+                            let vreg_statement = self.statement_list.get_vreg_statement_by_id(*other_statement_id);
+                            if let Some(vreg) = vreg_statement.p_def_x {
+                                if vregs_defined_in_this_blcok.contains(&vreg) {
+                                    acc.push(*other_statement_id);
+                                }
+                            }
+                        }
+                    }
+                    acc
+                });
+
+            kill_statement
+        }).collect::<Vec<Vec<StatementId>>>();
+
+        // 所有権の問題で切り分けている
+        for (basic_block, statement_id_list) in self.list.iter_mut().zip(statement_id_list) {
+            basic_block.vreg_state_set.set_kill(statement_id_list);
+        }
+
+        let mut reach_changed = true;
+
+        while reach_changed {
+            reach_changed = false;
+            let mut new_reaches = Vec::new();
+
+            // new_reach計算フェーズ
+            for basic_block in &self.list {
+                let a = basic_block.vreg_state_set.derive_new_reach(
+                    &basic_block.id, 
+                    &self);
+                new_reaches.push(a);
+            }
+
+            // for i in &new_reaches {
+            //     println!("hello world {:?}", i);
+            // }
+
+            // 更新フェーズ
+            for (basic_block, new_reach) in &mut self.list.iter_mut().zip(new_reaches) {
+                if set_neq(&basic_block.vreg_state_set.reach, &new_reach) {
+                    basic_block.vreg_state_set.reach = new_reach;
+                    reach_changed = true;
+                }
+            }
         }
 
         Ok(BasicBlockList {
             list: self.list,
+            statement_list: self.statement_list,
             _phantom: PhantomData 
         })
     }
@@ -288,6 +484,9 @@ impl BasicBlockList<IRSetted> {
 
 impl BasicBlockList<AllSetted> {
 
+    /// reachを導出する
+    pub fn derive_reach(mut self) {
+    }
 }
 
 #[derive(Hash, Copy, Clone, PartialEq, Eq, Debug)]
@@ -296,6 +495,138 @@ pub struct BasicBlockId(usize);
 impl BasicBlockId {
     pub fn new(id: usize) -> Self {
         Self(id)
+    }
+}
+
+// =================================================================================
+//                         到達する定義、生存区間解析用構造体
+// =================================================================================
+
+struct StatementSetting;
+struct StatementSetted;
+
+/// 使い方
+/// 命令列をみて、VregStatementを`gen_vreg_statement`を使ってセットしていく
+pub struct StatementList {
+    statements: Vec<VregStatement>, // You can access to this list by statement_id
+}
+
+impl StatementList {
+    fn gen_vreg_statement(&mut self, p_def_x: Option<VReg>, p_uses_x: Vec<VReg>) -> StatementId {
+        let id = StatementId(self.statements.len());
+        self.statements.push(VregStatement { id, p_def_x, p_uses_x });
+        id
+    }
+
+    fn get_vreg_statement_by_id(&self, statement_id: StatementId) -> &VregStatement {
+        &self.statements[statement_id.0]
+    }
+}
+
+#[derive(Clone, Copy)]
+struct StatementId(usize);
+
+/// p_def_x: 文pで変数xが定義されているとき,p(def(x))
+/// p_uses_x: 文pで変数xが使用されているとき,p(use(x))と書く
+pub struct VregStatement {
+    id: StatementId,
+    p_def_x: Option<VReg>,       // p(def(x))
+    p_uses_x: Vec<VReg>, // p(use(x)) なる vregの集合
+}
+
+// indexがStatementListと対応します
+// 
+pub struct VregStateSet {
+    reach: Vec<bool>,
+    /// DEF(B)の設定
+    /// ある変数xについてp(def(x)) ∈ Bで且つpの後にp'(def(x)) ∈ Bなるp'が無い
+    /// Bの中で定義され、Bの出口まで有効な文の集合
+    def: Vec<bool>,
+    /// Kill(B)の設定
+    /// 自分のブロック内で定義された変数を上書きするような文の集合
+    kill: Vec<bool>,
+}
+
+// VregStateSet helper
+fn derive_vreg_statement_id_from_bool_list(l: &[bool]) -> Vec<StatementId> {
+    l
+    .iter()
+    .enumerate()
+    .fold(Vec::<StatementId>::new(), |mut acc, (index, b)| {
+        if *b {
+            acc.push(StatementId(index));
+        }
+        acc
+    })
+}
+
+fn set_or (a: &[bool], b: &[bool]) -> Vec<bool> {
+    a.iter().zip(b).map(|(i, j)| *i || *j).collect()
+}
+
+fn set_minus (a: &[bool], b: &[bool]) -> Vec<bool> {
+    a.iter().zip(b).map(|(i, j)| *i && !*j).collect()
+}
+
+fn set_formula (a: &[bool], b: &[bool], c: &[bool]) -> Vec<bool> {
+    set_or(a, &set_minus(b, c))
+}
+
+fn set_neq(a: &[bool], b: &[bool]) -> bool {
+    a.iter().zip(b).any(|(i, j)| *i ^ *j)
+}
+
+impl VregStateSet {
+    // StatementListと同じ長さの配列で初期化
+    fn new(v: &StatementList) -> Self {
+        Self { // すべて空集合として設定する
+            reach: vec![false; v.statements.len()], 
+            def: vec![false; v.statements.len()],
+            kill: vec![false; v.statements.len()],
+        }
+    }
+
+    /// DEF(B1) = {p1, p2}
+    /// の場合
+    /// self.def = [false, true, true, false, ...];
+    fn set_def(&mut self, statement_id_list: Vec<StatementId>) {
+        for i in statement_id_list {
+            self.def[i.0] = true;
+        }
+    }
+
+    fn set_kill(&mut self, statement_id_list: Vec<StatementId>) {
+        for i in statement_id_list {
+            self.kill[i.0] = true;
+        }
+    }
+
+    // statement_idのリストを返す
+
+    pub fn get_def_statement_id_list(&self) -> Vec<StatementId> {
+        derive_vreg_statement_id_from_bool_list(&self.def)
+    }
+
+    pub fn get_kill_statement_id_list(&self) -> Vec<StatementId> {
+        derive_vreg_statement_id_from_bool_list(&self.kill)
+    }
+
+    fn derive_new_reach (&self, basic_block_id: &BasicBlockId, basic_block_list: &BasicBlockList<IRSetted>) -> Vec<bool> {
+        let current_basic_block = basic_block_list.get_basic_block(*basic_block_id);
+        let new_reach: Vec<bool> = current_basic_block
+            .pred // すべての先行ブロックについて、
+            .iter()
+            .fold(vec![false; basic_block_list.statement_list.statements.len()], |mut acc,pred_basic_block_id| {
+                let pred_block = basic_block_list.get_basic_block(*pred_basic_block_id);
+                acc = set_or(&set_formula(
+                    &pred_block.vreg_state_set.def,
+                    &pred_block.vreg_state_set.reach,
+                    &pred_block.vreg_state_set.kill
+                ), &acc);
+                acc
+            });
+    
+        new_reach
     }
 }
 
@@ -315,7 +646,10 @@ pub struct BasicBlock {
     pub pred: Vec<BasicBlockId>,
     pub succ: Vec<BasicBlockId>,
     pub insts: Vec<Instruction>,    // 中身の命令列（ここにはLabelDefは絶対入らない）
-    pub reach: HashSet<VReg>,
+    /// 「生存区間」、「到達する定義」のチェック
+    pub vreg_state_set: VregStateSet,
+    /// このブロック内に含まれる、変数を操作する文
+    pub statement_id_list: Vec<StatementId>,
 }
 
 impl BasicBlock {
@@ -338,6 +672,10 @@ impl BasicBlock {
         }
     }
 }
+
+// =================================================================================
+//                              Instruction及びIR
+// =================================================================================
 
 #[derive(Debug, Clone)]
 pub enum Instruction {
@@ -476,7 +814,7 @@ pub struct ModuleContext {
 #[cfg(test)]
 mod ir_ir_test {
     use crate::unit::size::Byte;
-    use crate::ir::ir::{BasicBlockList, ConstValue, Func, FuncDef, Instruction, Label, ModuleContext, Operand, Operator, RvIR};
+    use crate::ir::ir::{BasicBlockList, ConstValue, Instruction, Label, ModuleContext, Operand, Operator};
 
     #[test]
     fn test00 (){
@@ -485,7 +823,7 @@ mod ir_ir_test {
         let func_id = mod_ctx.create_func(
             "test_func",
             Byte::new(0),
-            Byte::new(0x10 * 5));
+            Byte::new(0x10 * 6));
 
         {
             let func = mod_ctx.get_func_mut(func_id).unwrap();
@@ -553,6 +891,9 @@ mod ir_ir_test {
                     println!("    basic_block id: {}", basic_block.id.0);
                     println!("    basic_block pred: {:?}", basic_block.pred);
                     println!("    basic_block succ: {:?}", basic_block.succ);
+                    println!("    reach {:?}", basic_block.vreg_state_set.reach);
+                    println!("    def {:?}", basic_block.vreg_state_set.def);
+                    println!("    kill {:?}", basic_block.vreg_state_set.kill);
                 }
             } else {
                 println!("failed to setting basic_block_list");
